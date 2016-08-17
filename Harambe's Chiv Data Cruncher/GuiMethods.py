c45be34f4628e92e7ce5598201742f3f72d091be
goldsugarwithce <Jacob@DESKTOP-V4NS6FU>
2016-08-17 07:26:54 -0400

    Connected Gui to Directory

---
+++ Harambe's Chiv Data Cruncher/GuiMethods.py
@@ -0,0 +1,10 @@
+'''
+Created on Aug 15, 2016
+
+@author: Jacob
+'''
+class GuiMethods():
+    def __init__(self, Directory):
+        self.Directory = Directory
+    def printShit(self):
+        print(self.Directory.teamDir)
